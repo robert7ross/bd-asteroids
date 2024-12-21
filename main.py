693c23ed8975d1cce1725d42537e4968f83cf981
robert7ross <rt.ross@outlook.com>
2024-12-20 19:47:40 -0600

    add player sprite class

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
-
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
     
 
@@ -19,7 +20,9 @@ def main():
                 return
             
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
 
 
